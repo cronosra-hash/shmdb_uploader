9e8648a10368c064162613fbc10de9f945d64834
cronosra-hash <cronosra@gmail.com>
2025-08-20 12:52:14 +0100

    Commit

--- main_tv.py
+++ main_tv.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from db.connection import get_connection
 from tmdb.tv_api import fetch_series
 from uploader.tv_uploader import insert_or_update_series_data
@@ -22,28 +21,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-from db.connection import get_connection
-from tmdb.tv_api import fetch_series
-from uploader.tv_uploader import insert_or_update_series_data
-
-SERIES_IDS = [39435, 90669]
-
-def main():
-    conn = get_connection()
-    try:
-        for sid in SERIES_IDS:
-            print(f"🔍 Fetching Series ID {sid}")
-            try:
-                series = fetch_series(sid)
-                insert_or_update_series_data(conn, series)
-            except Exception as e:
-                print(f"❌ Failed to process {sid}: {e}")
-                conn.rollback()
-    finally:
-        conn.close()
-        print("✅ Sync complete. Connection closed.")
-
-if __name__ == "__main__":
-    main()
->>>>>>> d9b5cc2 (first commit)
