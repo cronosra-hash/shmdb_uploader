18f1ccf1c01904b21c2627906f7df8ad1a928220
cronosra-hash <cronosra@gmail.com>
2026-02-04 18:10:35 +0000

    fixed some sql

--- queries/movies_missing_fields.sql
+++ queries/movies_missing_fields.sql
@@ -1,43 +1,41 @@
 SELECT
   COUNT(*) FILTER (
-    WHERE
-      overview IS NULL
+    WHERE overview IS NULL OR overview = ''
   ) AS missing_overview,
+
   COUNT(*) FILTER (
-    WHERE
-      release_date IS NULL
+    WHERE release_date IS NULL
   ) AS missing_release_date,
+
   COUNT(*) FILTER (
-    WHERE
-      runtime IS NULL
+    WHERE runtime IS NULL
   ) AS missing_runtime,
+
   COUNT(*) FILTER (
-    WHERE
-      poster_path IS NULL
+    WHERE poster_path IS NULL OR poster_path = ''
   ) AS missing_poster_path,
+
   COUNT(*) FILTER (
-    WHERE
-      original_language IS NULL
+    WHERE original_language IS NULL OR original_language = ''
   ) AS missing_original_language,
+
   COUNT(*) FILTER (
-    WHERE
-      status IS NULL
+    WHERE status IS NULL OR status = ''
   ) AS missing_status,
+
   COUNT(*) FILTER (
-    WHERE
-      imdb_id IS NULL
+    WHERE imdb_id IS NULL OR imdb_id = ''
   ) AS missing_imdb,
+
   COUNT(*) FILTER (
-    WHERE
-      budget IS NULL
+    WHERE budget IS NULL
   ) AS missing_budget,
+
   COUNT(*) FILTER (
-    WHERE
-      revenue IS NULL
+    WHERE revenue IS NULL
   ) AS missing_revenue,
+
   COUNT(*) FILTER (
-    WHERE
-      tagline IS NULL OR tagline LIKE 'Tagline%' or tagline = ''
+    WHERE tagline IS NULL
   ) AS missing_tagline
-FROM
-  movies;
+FROM movies;
