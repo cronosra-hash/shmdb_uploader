25cc5a51c1265fabceea8fc18d813c31903a3cc9
cronosra-hash <cronosra@gmail.com>
2026-02-28 14:22:39 +0000

    made cast and crew collapsable

--- db/helpers.py
+++ db/helpers.py
@@ -4,18 +4,21 @@ from db.connection import get_connection, release_connection
 
 @contextmanager
 def dict_cursor():
-    """
-    Provides a RealDictCursor with prepared statements enabled,
-    and guarantees the connection is returned to the pool.
-    """
     conn = get_connection()
+    cursor = None
     try:
         cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        cursor.prepare_threshold = 0  # Enable prepared statements
+        cursor.prepare_threshold = 0
         yield cursor
+        conn.commit()
+    except Exception:
+        conn.rollback()
+        raise
     finally:
-        try:
-            cursor.close()
-        except Exception:
-            pass
+        if cursor:
+            try:
+                cursor.close()
+            except Exception:
+                pass
         release_connection(conn)
+
